9dfb3d7effa32458e6c39f6f456e9f93b9249e38
Compro-Prasad <comproprasad@gmail.com>
2018-02-14 02:13:55 +0530

    First commit

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (js2-mode iedit iedit-mode multiple-cursors treemacs-projectile treemacs which-key ace-window sr-speedbar company-irony yasnippet-snippets helm-projectile projectile expand-region web-mode hungry-delete magit ggtags rtags yasnippet helm spacemacs-theme zenburn-theme ahungry-theme solarized-theme monokai-theme use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(region ((t (:background "pale turquoise" :distant-foreground "gtk_selection_fg_color")))))
