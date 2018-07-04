0ebb0499c0c235cbdccef165f88bfbbc8ca46497
Compro-Prasad <comproprasad@gmail.com>
2018-07-04 11:10:03 +0530

    Added bunch of useful packages
    - hungry-delete
    - magit
    - expand-region
    - projectile
    - switch-window
    - which-key
    - multiple-cursors
    - undo-tree
    - helpful
    - move-text

--- init.el
+++ init.el
@@ -21,3 +21,86 @@
 ;;;   Tell use-package to always use straight.el when installing packages
 (setq straight-use-package-by-default t)
 ;;;   end
+
+
+;;;   Hungry delete is the best part of editing text!
+(use-package hungry-delete
+  :init
+  (global-hungry-delete-mode t))
+;;;   end
+
+
+;;;   Magit for top notch git integration
+(use-package magit
+  :bind
+  (("C-x g" . magit-status)))
+;;;   end
+
+
+;;;   Expand region for smart region selection
+(use-package expand-region
+  :bind
+  (("C-=" . er/expand-region)
+   ("C-+" . er/contract-region)))
+;;;   end
+
+
+;;;   Project support is very useful
+(use-package projectile
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :config
+  (projectile-mode 1))
+;;;   end
+
+
+;;;   Switching windows is a bit hard in Emacs
+(use-package switch-window
+  :bind
+  (("M-\\" . switch-window)))
+;;;   end
+
+
+;;;   Nice to lookup new keys to learn new stuff
+(use-package which-key
+  :init
+  (which-key-mode 1))
+;;;   end
+
+
+;;;   Multiple cursor for small and fast edits
+(use-package multiple-cursors
+  :bind
+  (("C-S-c" . mc/edit-lines)
+   ("M-S-<up>" . mc/mark-previous-like-this)
+   ("M-<up>" . mc/skip-to-previous-like-this)
+   ("M-S-<down>" . mc/mark-next-like-this)
+   ("M-<down>" . mc/skip-to-next-like-this)
+   ("C-c C-<" . mc/mark-all-like-this)
+   ("M-S-<mouse-1>" . mc/add-cursor-on-click)
+   ("M-S-<mouse-2>" . mc/add-cursor-on-click)
+   ("M-S-<mouse-3>" . mc/add-cursor-on-click)))
+;;;   end
+
+
+;;;   Undo tree for better visualization of undo in Emacs
+(use-package undo-tree
+  :init
+  (global-undo-tree-mode t))
+;;;   end
+
+
+;;;   More verbose Emacs documentation lookup
+(use-package helpful
+  :bind
+  (("C-h f" . helpful-callable)
+   ("C-h v" . helpful-variable)
+   ("C-h k" . helpful-key)))
+;;;   end
+
+
+;;;   Move text in a buffer
+(use-package move-text
+  :bind (("C-_" . move-text-up)
+         ("C--" . move-text-down)))
+;;;   end
