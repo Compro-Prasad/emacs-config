7ce681a0f06cfb6c283486713f6c80e9b52b12da
Compro-Prasad <comproprasad@gmail.com>
2018-07-15 22:25:23 +0530

    I don't wan't package.el to load

---
+++ early-init.el
@@ -0,0 +1,4 @@
+;; Earlier Emacs versions do not load the early init file and do not initialize
+;; the package manager before loading the init file, so this file is neither
+;; needed nor loaded on those versions.
+(setq package-enable-at-startup nil)
