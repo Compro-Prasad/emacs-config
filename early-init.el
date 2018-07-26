775862a8c95d61543b40aa9c8b1936358d63a158
Compro-Prasad <comproprasad@gmail.com>
2018-07-26 21:41:35 +0530

    Added web-mode

--- early-init.el
+++ early-init.el
@@ -1,4 +1,2 @@
-;; Earlier Emacs versions do not load the early init file and do not initialize
-;; the package manager before loading the init file, so this file is neither
-;; needed nor loaded on those versions.
+;; For Emacs 27 and above
 (setq package-enable-at-startup nil)
