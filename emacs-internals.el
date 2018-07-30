ff6c9d61f2bf8ff0e52b3851e7f1307809570538
Compro-Prasad <comproprasad@gmail.com>
2018-07-30 22:52:20 +0530

    Cheated from spacemacs to set fonts more easily

--- emacs-internals.el
+++ emacs-internals.el
@@ -441,6 +441,94 @@ minibuffer using `display-startup-echo-area-message'.")
           ("False" .    #x1d53d)
           )))
 
+(defun mplist-remove (plist prop)
+  "Return a copy of a modified PLIST without PROP and its values.
+
+If there are multiple properties with the same keyword, only the first property
+and its values are removed."
+  (let ((tail plist)
+        result)
+    (while (and (consp tail) (not (eq prop (car tail))))
+      (push (pop tail) result))
+    (when (eq prop (car tail))
+      (pop tail)
+      (while (and (consp tail) (not (keywordp (car tail))))
+        (pop tail)))
+    (while (consp tail)
+      (push (pop tail) result))
+    (nreverse result)))
+
+(defun set-default-font (plists)
+  "Set the font given the passed PLISTS.
+
+PLISTS has either the form (\"fontname\" :prop1 val1 :prop2 val2 ...)
+or is a list of such. The first font that can be found will be used.
+
+The return value is nil if no font was found, truthy otherwise."
+  (unless (listp (car plists))
+    (setq plists (list plists)))
+  (catch 'break
+    (dolist (plist plists)
+      (when (find-font (font-spec :name (car plist)))
+        (let* ((font (car plist))
+               (props (cdr plist))
+               (font-props (mplist-remove
+                            ;; although this keyword does not exist anymore
+                            ;; we keep it for backward compatibility
+                            (mplist-remove props :powerline-scale)
+                            :powerline-offset))
+               (fontspec (apply 'font-spec :name font font-props)))
+          (set-frame-font fontspec nil t)
+          (push `(font . ,(frame-parameter nil 'font)) default-frame-alist)
+          (pcase system-type
+            (`gnu/linux
+             (setq fallback-font-name "NanumGothic")
+             (setq fallback-font-name2 "NanumGothic"))
+            (`darwin
+             (setq fallback-font-name "Arial Unicode MS")
+             (setq fallback-font-name2 "Arial Unicode MS"))
+            (`windows-nt
+             (setq fallback-font-name "MS Gothic")
+             (setq fallback-font-name2 "Lucida Sans Unicode"))
+            (`cygwin
+             (setq fallback-font-name "MS Gothic")
+             (setq fallback-font-name2 "Lucida Sans Unicode"))
+            (other
+             (setq fallback-font-name nil)
+             (setq fallback-font-name2 nil)))
+          (when (and fallback-font-name fallback-font-name2)
+            ;; remove any size or height properties in order to be able to
+            ;; scale the fallback fonts with the default one (for zoom-in/out
+            ;; for instance)
+            (let* ((fallback-props (mplist-remove
+                                    (mplist-remove font-props :size)
+                                    :height))
+                   (fallback-spec (apply 'font-spec
+                                         :name fallback-font-name
+                                         fallback-props))
+                   (fallback-spec2 (apply 'font-spec
+                                          :name fallback-font-name2
+                                          fallback-props)))
+              ;; window numbers
+              (set-fontset-font "fontset-default"
+                                '(#x2776 . #x2793) fallback-spec nil 'prepend)
+              ;; mode-line circled letters
+              (set-fontset-font "fontset-default"
+                                '(#x24b6 . #x24fe) fallback-spec nil 'prepend)
+              ;; mode-line additional characters
+              (set-fontset-font "fontset-default"
+                                '(#x2295 . #x22a1) fallback-spec nil 'prepend)
+              ;; new version lighter
+              (set-fontset-font "fontset-default"
+                                '(#x2190 . #x2200) fallback-spec2 nil 'prepend))))
+        (throw 'break t)))
+    nil))
+
+(set-default-font '("Ubuntu Mono"
+                    :size 16
+                    :weight normal
+                    :width normal))
+
 ;; No box around modeline
 (defun after-init-jobs ()
   "Configurations run after Emacs starts."
