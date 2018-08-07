6a8455c50383ee7b558c853a812fdbc64ec6689f
Compro-Prasad <comproprasad@gmail.com>
2018-08-08 02:36:08 +0530

    Updated .mc-lists.el

--- .mc-lists.el
+++ .mc-lists.el
@@ -11,9 +11,12 @@
         hungry-delete-forward
         kill-region
         markdown-outdent-or-delete
+        my-backward-kill-word
+        my-kill-word
         org-beginning-of-line
         org-self-insert-command
         sgml-slash
+        web-mode-comment-or-uncomment
         ))
 
 (setq mc/cmds-to-run-once
