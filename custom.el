91da5df665eafc8985bf0214a23ee5cf8039f061
Compro-Prasad <comproprasad@gmail.com>
2018-08-30 19:26:26 +0530

    Some directory local variables that are considered safe

--- custom.el
+++ custom.el
@@ -4,14 +4,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((web-mode-script-padding . 0)
+   '((web-mode-block-padding . 0)
+     (web-mode-style-padding . 0)
+     (web-mode-script-padding . 0)
      (web-mode-block-padding . 2)
      (web-mode-style-padding . 2)
      (web-mode-part-padding . 2)
      (c-file-offsets
       (substatement-open . 0)
       (arglist-intro . +)
-      (arglist-close . +)))))
+      (arglist-close . +))))
+ '(scroll-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
