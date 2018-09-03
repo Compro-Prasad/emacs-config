f3cdf9555c90d923d4320bb972085c80fdb97a13
Compro-Prasad <comproprasad@gmail.com>
2018-09-03 18:11:17 +0530

    Added dired-sidebar . It feels snappier.

--- init.el
+++ init.el
@@ -200,6 +200,29 @@
 ;;;   Sidebar
 (use-package treemacs
   :bind ("<f9> t" . treemacs))
+
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (use-package vscode-icon
+    :straight
+    (vscode-icon
+     :type git :host github
+     :repo "jojojames/vscode-icon-emacs")
+    :commands (vscode-icon-for-file))
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-theme 'vscode)
+  (setq dired-sidebar-use-term-integration t)
+  (setq dired-sidebar-use-custom-font t))
 ;;;   end
 
 
