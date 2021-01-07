b3cc1f9ed07b765210cc8fcd6bc4af53db58c8d4
Compro-Prasad <comproprasad@gmail.com>
2021-01-07 09:57:57 +0530

    Remove tramp slowdown on Linux (tested on WSL 2 Ubuntu)

--- init.el
+++ init.el
@@ -331,6 +331,37 @@ The return value is nil if no font was found, truthy otherwise."
 
 (leaf general :leaf-defer nil :ensure t :require t)
 
+(make-directory "~/.ssh/sockets" t)
+
+(setq tramp-ssh-controlmaster-options "")
+
+(defvar disable-tramp-backups '(all))
+
+(eval-after-load "tramp"
+  '(progn
+     ;; Modified from https://www.gnu.org/software/emacs/manual/html_node/tramp/Auto_002dsave-and-Backup.html
+     (setq backup-enable-predicate
+           (lambda (name)
+             (and (normal-backup-enable-predicate name)
+              ;; Disable all tramp backups
+              (and disable-tramp-backups
+                   (member 'all disable-tramp-backups)
+                   (not (file-remote-p name 'method)))
+              (not ;; disable backup for tramp with the listed methods
+               (let ((method (file-remote-p name 'method)))
+                 (when (stringp method)
+                   (member method disable-tramp-backups)))))))
+
+     (defun tramp-set-auto-save--check (original)
+       (if (funcall backup-enable-predicate (buffer-file-name))
+           (funcall original)
+         (auto-save-mode -1)))
+
+     (advice-add 'tramp-set-auto-save :around #'tramp-set-auto-save--check)
+
+     ;; Use my ~/.ssh/config control master settings according to https://puppet.com/blog/speed-up-ssh-by-reusing-connections
+     (setq tramp-ssh-controlmaster-options "")))
+
 (leaf tab-bar :leaf-defer nil :require t
   :when (> emacs-major-version 27)
   :bind (("C-t" . tab-bar-new-tab-event)
