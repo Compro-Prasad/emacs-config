9498b81706f820c4a8274c28485fe47a1b601e6c
Compro-Prasad <comproprasad@gmail.com>
2021-03-19 10:24:38 +0530

    Remove indent-tabs-mode t from mylinux C style

--- init.el
+++ init.el
@@ -871,7 +871,6 @@ The return value is nil if no font was found, truthy otherwise."
              '("linux"
                (tab-width . 4)
                (c-basic-offset . 4)
-               (indent-tabs-mode . t)
                (fill-column . 80)
                (c-hanging-semi&comma-criteria . my/c-semi&comma)
                (c-cleanup-list empty-defun-braces ;; {}
