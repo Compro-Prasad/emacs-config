ed9cd9585253ed7e16c297e9bd306efe5de0b9d1
Compro-Prasad <comproprasad@gmail.com>
2023-02-18 18:10:45 +0530

    Use python from MELPA

--- init.el
+++ init.el
@@ -1792,7 +1792,7 @@ buffer boundaries with possible narrowing."
   (setq treemacs-read-string-input 'from-minibuffer))
 
 (add-hook 'python-mode-hook (lambda () (setq-local fill-column 85)))
-(leaf python
+(leaf python :ensure t
   :bind ((python-mode-map
           ("TAB" . python-indent-shift-right)
           ("S-TAB" . python-indent-shift-left)
