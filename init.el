0151e1e765e73dbfbcd82e043755bcdc5460feb2
abhishek-compro <abhishek.prasad@noorahealth.org>
2024-09-26 13:56:18 +0530

    Add indent-bars and otpp

--- init.el
+++ init.el
@@ -2346,6 +2346,21 @@ References:
 
 (use-package yaml-mode :ensure t)
 
+(use-package indent-bars :ensure t)
+
+(use-package otpp
+  :ensure t
+  :after project
+  :init
+  ;; If you like to define some aliases for better user experience
+  (defalias 'one-tab-per-project-mode 'otpp-mode)
+  (defalias 'one-tab-per-project-override-mode 'otpp-override-mode)
+  ;; Enable `otpp-mode' globally
+  (otpp-mode 1)
+  ;; If you want to advice the commands in `otpp-override-commands'
+  ;; to be run in the current's tab (so, current project's) root directory
+  (otpp-override-mode 1))
+
 (set-face-attribute 'mode-line nil :box nil)
 (set-face-attribute 'mode-line-inactive nil :box nil)
 (when (> emacs-major-version 27)
