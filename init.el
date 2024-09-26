f517177c0665273ac99ea33754001493d0fbdcb3
abhishek-compro <abhishek.prasad@noorahealth.org>
2024-09-26 14:31:10 +0530

    Add support for showing font in emacs

--- init.el
+++ init.el
@@ -2368,6 +2368,8 @@ References:
          symbol-overlay-map
          ("C-o" . casual-symbol-overlay-tmenu)))
 
+(use-package show-font :ensure t)
+
 (set-face-attribute 'mode-line nil :box nil)
 (set-face-attribute 'mode-line-inactive nil :box nil)
 (when (> emacs-major-version 27)
